d07ea66d39528d6d2d7adf090bf5734ac3a05ae6
vale-c <valent95@gmail.com>
2024-05-04 11:57:56 +0200

    updated stoic quote positioning

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -75,7 +75,7 @@ const App = () => {
             {hours}:{minutes}
           </h1>
           {quote.author && quote.text && (
-            <div className="mx-8 mt-16 max-w-xl rounded-lg bg-black/40 sm:mx-0">
+            <div className="absolute bottom-4 mx-8 mt-16 max-w-xl rounded-lg bg-black/40 sm:mx-0">
               <blockquote className="p-3 text-center text-xl italic text-white shadow-xl">
                 “{quote.text}”
                 <footer className="mt-2 text-lg font-semibold text-white">
