170d65052bf3a800223ca21cd5a7bf8db0f064bc
Valentina Calabrese <valentina.calabrese@remote.com>
2024-05-07 17:23:14 +0200

    changing cache duration

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -17,7 +17,7 @@ type WeatherData = {
   fetchedAt: number
 }
 
-const CACHE_DURATION = 30 * 60 * 1000
+const CACHE_DURATION = 5 * 60 * 1000 // 5 minutes
 
 const getWeatherIcon = (weatherCode: number) => {
   switch (weatherCode) {
